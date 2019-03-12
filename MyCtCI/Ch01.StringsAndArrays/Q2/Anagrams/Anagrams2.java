b8b53dd834c22039241de78670951665dd0681f1
Surya <surya_uppuluri@dellteam.com>
2019-03-12 21:55:40 +0530

    Chapter 1

---
+++ MyCtCI/Ch01.StringsAndArrays/Q2/Anagrams/Anagrams2.java
@@ -0,0 +1,36 @@
+package Q2.Anagrams;
+
+public class Anagrams2 {
+
+	public static void main(String[] args) {
+
+		String[][] namePairStrings = { { "surya", "ayrus" }, { "Sushmitha", "ahthmhsus" } };
+		for (String[] namePair : namePairStrings) {
+			String first = namePair[0];
+			String second = namePair[1];
+			System.out.println(checkAnag(first, second));
+
+		}
+	}
+
+	private static boolean checkAnag(String first, String second) {
+
+		int letters[] = new int[128];
+		if (first.length() != second.length())
+			return false;
+		for (int i = 0; i < first.length(); i++) {
+			letters[first.charAt(i)]++;
+		}
+
+		for (int i = 0; i < second.length(); i++) {
+			letters[second.charAt(i)]--;
+			if (letters[second.charAt(i)] < 0) {
+				{
+					System.out.println("Broken at " + second.charAt(i)+ " " + i);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
---
+++ MyCtCI/Ch01.StringsAndArrays/Q3/URLify/Anagrams1.java
@@ -0,0 +1,32 @@
+package Q3.URLify;
+
+import java.util.Arrays;
+
+public class Anagrams1 {
+
+	public static void main(String[] args) {
+
+		String[][] namePairStrings = { { "surya", "aYrus" }, { "Sushmitha", "Ahtimhsus" } };
+		for (String[] namePair : namePairStrings) {
+			String first = namePair[0];
+			String second = namePair[1];
+			System.out.println(sortCheckAnag(first, second));
+
+		}
+	}
+
+	private static boolean
+	sortCheckAnag(String first, String second) {
+		char[] firstC = first.toLowerCase().toCharArray(); 
+		char[] secondC = second.toLowerCase().toCharArray();
+		if(sort(firstC).equals(sort(secondC)))
+		return true;
+		else return false;
+	}
+
+	private static String sort(char[] charArray) {
+		Arrays.sort(charArray);
+		return new String(charArray);
+	}
+	
+}
